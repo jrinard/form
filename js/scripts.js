d5ef9f27224e4b40be0afc93173814cc33b4aec9
Joshua Rinard and Luke Keysboe <joshua@>
2016-10-19 10:24:06 -0700

    Add comments and uppercase

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var person1Input = $("input#person1").val();
+    var person1Input = $("input#person1").val().toUpperCase(); //looks for the "id person" inside the input html. Then creates a method that sets the inputted value and assigns that to a variable named "person1Input"
     var person2Input = $("input#person2").val();
     var animalInput= $("input#animal").val();
     var exclamationInput = $("input#exclamation").val();
     var verbInput = $("input#verb").val();
     var nounInput = $("input#noun").val();
 
-    $(".person1").text(person1Input);
+    $(".person1").text(person1Input); //this jQuery function selects the "person1 class inside the span tag. Then it runs the text method, which converts the variable to text "
     $(".person2").text(person2Input);
     $(".animal").text(animalInput);
     $(".exclamation").text(exclamationInput);
@@ -18,4 +18,5 @@ $(document).ready(function() {
 
     event.preventDefault();
   });
+
 });
